dda34a064653e6ee7d0c47f739e57a9b85ca637a
Grécio Diogo <fonebahia8@gmail.com>
2025-04-24 10:22:04 +0100

    feat: add pie-chart deps and associate on home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { MainComponent } from './pages/main/main.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 
 @NgModule({
-  declarations: [MainComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  declarations: [MainComponent, PieChartComponent, BarChartComponent],
+  imports: [CommonModule, HomeRoutingModule, NgApexchartsModule],
 })
 export class HomeModule {}
