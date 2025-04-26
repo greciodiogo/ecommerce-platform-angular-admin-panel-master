e58ca25ff7abd4f7a38e6b54066a10bc50d04807
Grécio Diogo <fonebahia8@gmail.com>
2025-04-26 23:59:41 +0100

    feat: add sales

---
+++ src/app/sales/pages/sales-list/sales-list.component.spec.ts
@@ -0,0 +1,131 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SalesListComponent } from './sales-list.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { provideMockStore } from '@ngrx/store/testing';
+import { selectOrdersListWithItems } from '../../store';
+import {
+  MatRowHarness,
+  MatTableHarness,
+} from '@angular/material/table/testing';
+import { DatePipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { FormatCurrencyPipe } from '../../../shared/pipes/format-currency.pipe';
+import { selectSettingsList } from '../../../settings/store';
+
+describe('SalesListComponent', () => {
+  let component: SalesListComponent;
+  let fixture: ComponentFixture<SalesListComponent>;
+  let loader: HarnessLoader;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatCardModule,
+        MatTableModule,
+        MatIconModule,
+      ],
+      declarations: [SalesListComponent, FormatCurrencyPipe],
+      providers: [
+        provideMockStore({
+          selectors: [
+            {
+              selector: selectOrdersListWithItems,
+              value: [
+                {
+                  id: 1,
+                  created: '2022-08-21T16:21:38.277Z',
+                  status: 'pending',
+                  fullName: 'Test Test',
+                  contactEmail: 'test@test.local',
+                  contactPhone: '123456789',
+                  items: [
+                    {
+                      id: 1,
+                      quantity: 2,
+                      price: 2,
+                    },
+                  ],
+                  itemsCount: 2,
+                  itemsTotal: 4,
+                  payment: {
+                    method: {
+                      name: 'payment-test',
+                      price: 2,
+                    },
+                  },
+                  delivery: {
+                    method: {
+                      name: 'delivery-test',
+                      price: 1,
+                    },
+                  },
+                },
+              ],
+            },
+            {
+              selector: selectSettingsList,
+              value: [
+                {
+                  id: 1,
+                  name: 'Currency',
+                  value: 'EUR',
+                },
+              ],
+            },
+          ],
+        }),
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SalesListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render table', async () => {
+    const table = await loader.getHarness(MatTableHarness);
+    expect(table).toBeTruthy();
+    const [row] = await table.getHeaderRows();
+    expect(await row.getCellTextByIndex()).toEqual([
+      'ID',
+      'Created',
+      'Status',
+      'Items',
+      'Total price',
+      'Full name',
+      'Delivery',
+      'Payment',
+    ]);
+  });
+
+  it('should render order row', async () => {
+    const row = await loader.getHarness(MatRowHarness);
+    expect(row).toBeTruthy();
+    expect(await row.getCellTextByIndex()).toEqual([
+      '1',
+      new DatePipe('en-US').transform(
+        '2022-08-21T16:21:38.277Z',
+        'd MMM yyyy, HH:mm',
+      ) ?? '',
+      'pending',
+      '2',
+      '€7.00',
+      'Test Test',
+      'delivery-test',
+      'payment-test',
+    ]);
+  });
+});
---
+++ src/app/sales/pages/sales-list/sales-list.component.ts
@@ -0,0 +1,50 @@
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Store } from '@ngrx/store';
+import { OrdersActions, selectOrdersListWithItems } from '../../store';
+import { MatTableDataSource } from '@angular/material/table';
+import { Order } from '../../../core/api';
+import { firstValueFrom, Subscription } from 'rxjs';
+import { MatSort } from '@angular/material/sort';
+import { ProductsActions } from '../../../catalog/store';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-orders-list',
+  templateUrl: './orders-list.component.html',
+  styleUrls: ['./orders-list.component.scss'],
+})
+export class SalesListComponent implements OnInit, AfterViewInit, OnDestroy {
+  sales$ = this.store.select(selectOrdersListWithItems);
+  dataSource = new MatTableDataSource<Order>();
+  subscription!: Subscription;
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.dataSource.data = [];
+    this.store.dispatch(ProductsActions.loadProducts());
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+  async ngAfterViewInit() {
+    this.dataSource.data = await firstValueFrom(this.sales$);
+    this.subscription = this.sales$.subscribe((orders) => {
+      this.dataSource.data = orders;
+    });
+    this.store.dispatch(OrdersActions.loadOrders());
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+}
