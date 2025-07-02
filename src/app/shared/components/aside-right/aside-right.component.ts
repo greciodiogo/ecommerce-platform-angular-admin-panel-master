0d7ea4bf49dfde7ed289dc7eca2e89dff8c93012
Grécio Diogo <fonebahia8@gmail.com>
2025-07-02 19:34:39 +0100

    fix: adjust component animation

--- src/app/shared/components/aside-right/aside-right.component.ts
+++ src/app/shared/components/aside-right/aside-right.component.ts
@@ -1,9 +1,22 @@
 import { Component, ElementRef, Input, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-aside-right',
   templateUrl: './aside-right.component.html',
-  styleUrls: ['./aside-right.component.css']
+  styleUrls: ['./aside-right.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({ right: '0', opacity: 1 })),
+      state('out', style({ right: '-250px', opacity: 0 })), // match .size_medium width
+      transition('out => in', [
+        animate('300ms cubic-bezier(0.4,0,0.2,1)')
+      ]),
+      transition('in => out', [
+        animate('200ms cubic-bezier(0.4,0,0.2,1)')
+      ]),
+    ])
+  ]
 })
 export class AsideRightComponent implements OnInit {
   @Input() componentTitle: string = ""
@@ -28,8 +41,8 @@ export class AsideRightComponent implements OnInit {
   }
   
   onClose(){
-    // this.close.emit();
-    this.isOpen = false
+    this.isOpen = false;
+    this.close.emit(); // Notify parent!
   }
 
   // public getComponentSize(){
