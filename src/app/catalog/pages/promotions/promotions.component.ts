ee6199d3a100b460b35e7b00d66c7b135b2f4211
Grécio Diogo <grecio.diogo@ideiasdinamicas.com>
2026-04-23 14:57:44 +0100

    fix: fix default promotion component

--- src/app/catalog/pages/promotions/promotions.component.ts
+++ src/app/catalog/pages/promotions/promotions.component.ts
@@ -1,30 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { CategoriesActions, selectCategoriesList, selectSelectedCategory } from '../../store';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-promotions',
   templateUrl: './promotions.component.html',
   styleUrls: ['./promotions.component.scss'],
 })
-export class PromotionsComponent implements OnInit, OnDestroy {
-  promotionCategory$ = this.store.select(selectSelectedCategory);
-  private categoriesSub?: Subscription;
-
-  constructor(private store: Store) {}
-
-  ngOnInit() {
-    this.categoriesSub = this.store.select(selectCategoriesList).subscribe(categories => {
-      const promoCat = categories.find(c => c.name?.toLowerCase() === 'promotions');
-      if (promoCat) {
-        this.store.dispatch(CategoriesActions.selectCategory({ categoryId: promoCat.id }));
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.categoriesSub) this.categoriesSub.unsubscribe();
-    this.store.dispatch(CategoriesActions.selectCategory({ categoryId: null }));
-  }
+export class PromotionsComponent {
+  constructor() {}
 }
